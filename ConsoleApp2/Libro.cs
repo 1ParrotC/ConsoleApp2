fcb554ccb776499bbbb2b6c64c725e179a6c9c9e
ParrotC <pablocrespobles@gmail.com>
2026-05-10 11:34:32 +0200

    Ejercicio1

---
+++ ConsoleApp2/Libro.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace ConsoleApp2
+{
+    public class Libro
+    {
+        private string titulo;
+        private string autor;
+        private int anyo;
+        private bool disponible;
+
+        public Libro(string titulo, string autor, int anyo, bool disponible)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.anyo = anyo;
+            this.disponible = disponible;
+        }
+
+        public string Titulo => titulo;
+        public string Autor => autor;
+        public int Anyo => anyo;
+        public bool Disponible => disponible;
+
+        public override string ToString()
+        {
+            return $"{Titulo} - {Autor} ({Anyo})";
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Ejemplo de uso de la clase Libro
+            var libro = new Libro("Cien años de soledad", "Gabriel García Márquez", 1967, true);
+            Console.WriteLine(libro.ToString());
+        }
+    }
+}
